86bd40f410b0bb12133531e8fe3a26841f411a8d
vo01github <hxt2013gg@gmail.com>
2015-03-19 19:30:33 +0800

    reading notes

---
+++ 《C陷阱与缺陷》/Chapter01.h
@@ -0,0 +1,13 @@
+﻿
+
+// 第1章 词法"陷阱"
+class chapter01
+{
+public:
+
+	void charAndString(void);	// 1.5 字符与字符串
+
+	void exercise13(void);		// 练习题1-3
+	void exercise14(void);		// 练习题1-4
+
+};
---
+++ 《C陷阱与缺陷》/Chapter02.h
@@ -0,0 +1,14 @@
+﻿
+
+
+// 第2章 语法"陷阱"
+class chapter02
+{
+public:
+	void declarationFunction(void);	// 2.1 理解函数声明
+	
+	void operatorPriority(void);	// 2.2 运算符的 优先级问题
+
+	void switchSentence(void);		// 2.4 switch 语句
+
+};
