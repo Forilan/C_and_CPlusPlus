8fde648d446b7e9b4179028b0c49d8652507d787
vo01github <hxt2013gg@gmail.com>
2015-03-19 20:00:44 +0800

    update

---
+++ C-Summary/(L-value)And(R-value).cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include "(L-value)And(R-value).h"
+
+
+
+
+void LvalueAndRvalue::aboutaddadd( void )
+{
+	int i = 1;
+
+	++i;		// 等价于下面这个函数
+	addaddVar(i);
+
+	i++;		// 等价于下面这个函数
+	VarAddAdd(i);
+}
+
+int LvalueAndRvalue::addaddVar( int &i )
+{
+	i = i+1;
+	return i;
+}
+
+int LvalueAndRvalue::VarAddAdd( int &i )
+{	
+	int temp;
+	temp = i;
+	i = i + 1;
+	return temp;
+}
+
+
