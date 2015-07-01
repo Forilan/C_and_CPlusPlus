e7661d0ec31ec46165aa096b90d47dcf91ed9d05
vo01github <hxt2014bbg@icloud.com>
2015-07-01 22:10:50 +0800

    1

---
+++ 【C++STL】/【容器】-操作-find.cpp
@@ -0,0 +1,19 @@
+﻿
+// 使用容器
+#include <vector>
+
+// 使用算法
+#include <algorithm>
+
+using std::vector;
+
+
+
+// ____________________________________________________  find() ____________________________________________________ 
+/*
+	http://www.cnblogs.com/web100/archive/2012/12/02/cpp-string-find-npos.html
+
+	C++中string.find()函数与string::npos	
+*/
+
+
