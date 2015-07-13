8a76c13d65e79a48ad1667ab6a831b6b0e3edce4
vo01github <hxt2014bbg@icloud.com>
2015-07-13 18:39:46 +0800

    2

--- 【C++STL】/【容器】-操作-find.cpp
+++ 【C++STL】/【容器】-操作-find.cpp
@@ -17,3 +17,16 @@ using std::vector;
 */
 
 
+/*
+STL之 眼花缭乱的string find 函数 [find find_first_of]  
+
+http://youanke2008.blog.163.com/blog/static/66887676201061515657619/
+
+find				查找
+rfind				反向查找
+find_first_of		查找包含子串中的任何字符，返回第一个位置
+find_first_not_of	查找不包含子串中的任何字符，返回第一个位置
+find_last_of		查找包含子串中的任何字符，返回最后一个位置
+find_last_not_of	查找不包含子串中的任何字符，返回最后一个位置
+*/ 
+
