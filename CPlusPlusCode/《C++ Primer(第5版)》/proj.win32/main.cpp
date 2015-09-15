b5ba442807285efbe149bc1e471dbe7da262be46
vo01github <hxt2014bbg@icloud.com>
2015-09-15 18:29:33 +0800

    3

--- CPlusPlusCode/《C++ Primer(第5版)》/proj.win32/main.cpp
+++ CPlusPlusCode/《C++ Primer(第5版)》/proj.win32/main.cpp
@@ -7,15 +7,15 @@
 #if 0		
 
 #include "01-第1章-开始.h"
-
+#include "02-第2章-变量和基本类型.h"
 #endif
 
 
-#include "02-第2章-变量和基本类型.h"
+#include "03-第3章-字符串、向量和数组.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	const_referenece_study1();
+	vector_study_other_operational();
 
 	getchar();		// 防止 窗口闪掉
 
