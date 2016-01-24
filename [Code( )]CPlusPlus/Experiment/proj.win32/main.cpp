ce5577a1de9b9fa82179fcde70dc783aad9f4383
vo01github <hxt2014bbg@icloud.com>
2016-01-24 18:43:37 +0800

    4

---
+++ [Code( )]CPlusPlus/Experiment/proj.win32/main.cpp
@@ -0,0 +1,32 @@
+// NamedPipeClient.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+#include "2char拼接成int.h"
+#include "200次for循环测量时间.h"
+
+#if 1
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+#if 1
+	char a = 0x63;
+	char b = 0xFD;
+	int res = char2int(a,b);
+	cout << res;
+#endif
+
+
+	//	for200times();
+
+	//getchar();		// 防止 窗口闪掉
+	system("pause");
+
+	return 0;
+}
+#endif
+
+
--- [Code( )]CPlusPlus/《C++ Primer(第5版)》/proj.win32/main.cpp
+++ [Code( )]CPlusPlus/《C++ Primer(第5版)》/proj.win32/main.cpp
@@ -22,21 +22,12 @@
 #endif
 
 #include "12-第12章-动态内存.h"
-#include "2char拼接成int.h"
-#include "200次for循环测量时间.h"
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 
-#if 1
-	char a = 0x63;
-	char b = 0xFD;
-	int res = char2int(a,b);
-	cout << res;
-#endif
 
-
-//	for200times();
 
 
 	getchar();		// 防止 窗口闪掉
