6561965ed238ecc59867ffe7b5cb68be8ef34d39
vo01github <hxt2014bbg@icloud.com>
2016-01-24 21:24:22 +0800

    1

--- [Code( )]CPlusPlus/Experiment/proj.win32/main.cpp
+++ [Code( )]CPlusPlus/Experiment/proj.win32/main.cpp
@@ -12,7 +12,7 @@
 int _tmain(int argc, _TCHAR* argv[])
 {
 
-#if 1
+#if 0
 	char a = 0x63;
 	char b = 0xFD;
 	int res = char2int(a,b);
@@ -23,6 +23,19 @@ int _tmain(int argc, _TCHAR* argv[])
 	//	for200times();
 
 	//getchar();		// ·ÀÖ¹ ´°¿ÚÉÁµô
+
+	int negaive = -2;
+	unsigned int er = 0 , ers = 0, erh = 0;
+
+	ers = negaive;
+	er = (unsigned int) negaive;
+	int h = er + negaive;
+
+	cout << ers << endl;
+	cout << h << endl;
+
+	cout << erh -2 << endl;
+
 	system("pause");
 
 	return 0;
