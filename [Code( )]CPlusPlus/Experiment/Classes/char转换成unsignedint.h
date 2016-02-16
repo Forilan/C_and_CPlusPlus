8abedb5f114dcbfdc52ae68fe068641733f8acc5
vo01github <hxt2014bbg@icloud.com>
2016-02-16 12:13:50 +0800

    2

--- [Code( )]CPlusPlus/Experiment/Classes/char转换成unsignedint.h
+++ [Code( )]CPlusPlus/Experiment/Classes/char转换成unsignedint.h
@@ -20,11 +20,14 @@ void runCharChangeInt()
 {
 	char a = 0x63;
 	char b = 0xFD;
-	unsigned int res =charChangeInt(b);
-	unsigned int res1 =charChangeInt(a);
-	//int res = char2int(a,b);
-	//int res1 = (int)char2short(a,b);
+	//unsigned int res =charChangeInt(b);
+	//unsigned int res1 =charChangeInt(a);
+	int res = char2int(a,b);
+	int res1 = (int)char2short(a,b);
 
 	cout << res << endl;
 	cout << res1 << endl;
+	cout << sizeof(char) << endl;
+	cout << sizeof(short) << endl;
+	cout << sizeof(int) << endl;
 }
